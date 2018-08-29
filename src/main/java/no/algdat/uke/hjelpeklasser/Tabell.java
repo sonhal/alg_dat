4441bc9c97afc4c77eeea91d04cff9809002ea79
sonhal <sondre.hal@gmail.com>
2018-08-29 21:44:56 +0200

    Added Tabell class

---
+++ src/main/java/no/algdat/uke/hjelpeklasser/Tabell.java
@@ -0,0 +1,75 @@
+package no.algdat.uke.hjelpeklasser;
+import java.util.*;
+
+public class Tabell {
+
+    private Tabell(){}
+
+    public static void bytt(int[] a, int i, int j)
+    {
+        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
+
+    public static int[] randPerm(int n)  // en effektiv versjon
+    {
+        Random r = new Random();         // en randomgenerator
+        int[] a = new int[n];            // en tabell med plass til n tall
+
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        {
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
+    }
+
+
+
+    public static void randPerm(int[] a)  // stokker om a
+    {
+        Random r = new Random();     // en randomgenerator
+
+        for (int k = a.length - 1; k > 0; k--)
+        {
+            int i = r.nextInt(k + 1);  // tilfeldig tall fra [0,k]
+            bytt(a,k,i);
+        }
+    }
+
+    public static int maks(int[] a, int fra, int til)
+    {
+        if (fra < 0 || til > a.length || fra >= til)
+        {
+            throw new IllegalArgumentException("Illegalt intervall!");
+        }
+
+        int m = fra;              // indeks til største verdi i a[fra:til>
+        int maksverdi = a[fra];   // største verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+        {
+            if (a[i] > maksverdi)
+            {
+                m = i;                // indeks til største verdi oppdateres
+                maksverdi = a[m];     // største verdi oppdateres
+            }
+        }
+
+        return m;  // posisjonen til største verdi i a[fra:til>
+    }
+
+    public static int maks(int[] a)  // bruker hele tabellen
+    {
+        return maks(a,0,a.length);     // kaller metoden over
+    }
+
+
+    public static int min(int[] a, int fra, int til) {
+
+        return 0;
+    }
+
+}
---
+++ src/test/java/no/algdat/uke/hjelpeklasser/TabellTest.java
@@ -0,0 +1,37 @@
+package no.algdat.uke.hjelpeklasser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TabellTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void bytt() {
+    }
+
+    @Test
+    void randPerm() {
+    }
+
+    @Test
+    void randPerm1() {
+    }
+
+    @Test
+    void maks() {
+    }
+
+    @Test
+    void maks1() {
+    }
+
+    @Test
+    void min() {
+    }
+}
